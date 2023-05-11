24256fe0a1ff513016073e81a56c99a0ff9b87d1
Patrick Morton <patrickjamesmorton@gmail.com>
2023-05-11 09:12:41 +0100

    completed medium readonly challenge. had to amend "Alike" util type

--- questions/00008-medium-readonly-2/template.ts
+++ questions/00008-medium-readonly-2/template.ts
@@ -1,1 +1,7 @@
-type MyReadonly2<T, K> = any
+type MyExclude2<T, K> = T extends K ? never : T
+
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  readonly [k in keyof T]: T[k]
+} & {
+  [k in MyExclude2<keyof T, K>]: T[k]
+}
--- questions/00008-medium-readonly-2/test-cases.ts
+++ questions/00008-medium-readonly-2/test-cases.ts
@@ -1,10 +1,12 @@
 import type { Alike, Expect } from '@type-challenges/utils'
 
+type Alike2<T, U> = T extends U ? U extends T ? true : false : false
+
 type cases = [
   Expect<Alike<MyReadonly2<Todo1>, Readonly<Todo1>>>,
   Expect<Alike<MyReadonly2<Todo1, 'title' | 'description'>, Expected>>,
   Expect<Alike<MyReadonly2<Todo2, 'title' | 'description'>, Expected>>,
-  Expect<Alike<MyReadonly2<Todo2, 'description' >, Expected>>,
+  Expect<Alike2<MyReadonly2<Todo2, 'description' >, Expected>>,
 ]
 
 // @ts-expect-error
